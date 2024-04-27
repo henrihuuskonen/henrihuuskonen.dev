376223dadfdebe3bccb79db6700a241069d70f7b
Henri Huuskonen <henri@swappie.com>
2024-04-27 22:05:36 +0300

    Update texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,34 +20,11 @@ const Home = () => {
                 <Hero/>
                 <InlineMenu/>
             </div>
-            <TextSection title="Software engineer" text={`Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
-                        of
-                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure
-                        Latin
-                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
-                        1.10.32
-                        and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-                        written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
-                        Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a
-                        line in
-                        section 1.10.32.`}
+            <TextSection title="Software engineer" text={`Experienced Senior Software Engineer with a knack for optimizing business operations through efficient software solutions. Over 5 years as a Software Engineer, I've built integrations to logistics providers, developed customer portals, and streamlined internal processes. Proficient in a diverse tech stack including React, NodeJS, Typescript, and Python, I thrive on tackling complex challenges and iterating on user experiences by making data-driven decisions through A/B testing. With a background in small electronics repair and customer service, I bring hands-on experience and a practical approach to software development.`}
             />
             {/*<Projects/>*/}
-            <TextSection title="Tinkerer" text={`Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
-                        of
-                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure
-                        Latin
-                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
-                        1.10.32
-                        and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-                        written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
-                        Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a
-                        line in
-                        section 1.10.32.`}
+            <TextSection title="Tinkerer" text={`I've always had a fascination with how things, including websites and electronics, operate. My hobby of fixing mobile devices in my free time while attending school helped me land a job as a mobile technician. After that, I found myself working for a start-up in a more factory-like environment with countless automation opportunities. I eventually came up with a number of software tools for automation that minimise human error and boost throughput. That helped me land a position on the engineering team of the company, and the rest is history.
+I still enjoy creating things with my hands now, outside of work, and I've been working on small IoT projects and fixing an old sailboat.`}
                          titleOnRight
             />
             <Footer/>
