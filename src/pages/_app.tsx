b5933f22e345e3426a75f14e28c75cf19a928be4
Henri Huuskonen <henri@swappie.com>
2024-05-03 14:39:20 +0300

    Move content to own file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,35 @@ import {ThemeProvider as NextThemesProvider} from 'next-themes';
 import {Analytics} from '@vercel/analytics/react';
 import React from "react";
 import {SpeedInsights} from "@vercel/speed-insights/next";
+import {META_DESCRIPTION} from "@/content";
+import Head from "next/head";
 
 const App = ({Component, pageProps}: AppProps) => {
     return <NextThemesProvider
         defaultTheme="dark"
         attribute="class"
     >
+        <Head>
+            <meta property="og:title" content="Henri Huuskonen - Software Engineer"/>
+            <meta
+                property="og:description"
+                content={META_DESCRIPTION}
+            />
+            <meta
+                property="og:image"
+                content="https://henrihuuskonen.dev/henri-huuskonen-avatar.svg"
+            />
+            <meta property="og:type" content="website"/>
+            <meta
+                property="og:url"
+                content="https://henrihuuskonen.dev/"
+            />
+            <link
+                rel="canonical"
+                href="https://henrihuuskonen.dev/"
+                key="canonical"
+            />
+        </Head>
         <Component {...pageProps} />
         <Analytics/>
         <SpeedInsights/>
