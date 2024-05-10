acaf3cf185820a25a74b5407e43703784414d6dc
Henri Huuskonen <henri@swappie.com>
2024-05-10 09:37:09 +0300

    Grep gh pr checks output to check if test and lint passed

--- .github/approve-and-merge.sh
+++ .github/approve-and-merge.sh
@@ -2,29 +2,19 @@
 
 PR_NUMBER=$1
 
-# Wait for the PR checks to complete
-gh pr checks $PR_NUMBER --watch --fail-fast
+# Check lines containing lint or playwright-tests and containing the word pass
+PR_CHECK_COMMAND=$(gh pr checks $PR_NUMBER)
+lint_check=$(echo "$COMMAND" | grep -E '^lint.*pass' | wc -l)
+playwright_tests_check=$(echo "$COMMAND" | grep -E '^playwright-tests.*pass' | wc -l)
 
-# Fetch the JSON data using 'gh pr view' command
-json_data=$(gh pr view $PR_NUMBER --json statusCheckRollup,mergeable)
 
-# Check if the command was successful
-if [ $? -ne 0 ]; then
-    echo "Error fetching PR data"
-    exit 1
-fi
-
-# Extract the "conclusion" fields from the JSON data
-conclusions=$(echo "$json_data" | jq -r '.statusCheckRollup[].conclusion')
-mergeable=$(echo "$json_data" | jq -r '.mergeable')
-
-# Check if all conclusions are "SUCCESS"
-if [[ $(echo "$conclusions" | grep -c "SUCCESS") -eq $(echo "$conclusions" | wc -l) && "$mergeable" == "MERGEABLE" ]]; then
-    echo "All conclusions are SUCCESS and PR is MERGEABLE"
+# Exit code based on the result
+if [ $lint_check -eq 1 ] && [ $playwright_tests_check -eq 1 ]; then
+    echo "Success: lint and playwright-tests both passed."
     gh pr review $PR_NUMBER --approve
     gh pr merge $PR_NUMBER --squash
     exit 0
 else
-    echo "Some conclusions are not SUCCESS or not MERGEABLE"
+    echo "Failure: lint and/or playwright-tests did not pass."
     exit 1
 fi
